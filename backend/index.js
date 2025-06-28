8cd3ccd5de75673280fff54b681f80e4a516b7fd
Kingston Davies <kingstond22@meta.com>
2025-06-27 21:25:20 -0700

    implemented backend routes to get and add internships via internship api

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
-const express = require('express')
+const express = require("express")
+const cors = require('cors');
+const profileRoutes = require('./routes/profiles');
+const internshipsRoutes = require('./routes/internships');
+
 const app = express()
-const PORT = 3000
+const PORT = 4000
 
 app.use(express.json())
+app.use(cors())
+
+app.use("/api/profiles", profileRoutes)
+app.use("/api/internships", internshipsRoutes)
+
 
 app.get('/', (req, res)=> {
     res.send('UpliftED API is running')
