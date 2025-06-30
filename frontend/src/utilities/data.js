c2c4508d35304957123937a26a2fb0bab9e646a7
Kingston Davies <kingstond22@meta.com>
2025-06-30 14:41:44 -0700

    utilities file for API calls to backend

---
+++ frontend/src/utilities/data.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+const backend = import.meta.env.VITE_SERVER_URL
+const API = axios.created({
+    baseURL: backend || 'http://localhost:4000/api'
+});
+
+//Profiles
+export const getProfile = () => API.get('/profiles/me');
+export const updateProfile = () => API.put('/profiles/me', data)
+
+//Internships
+export const getInternships = () => API.get('/internships');
+export const addInternship = () => async (internshipData) => {
+    const res = await API.post('/internships/userInput', internshipData);
+    return res;
+}
+
+export default API
