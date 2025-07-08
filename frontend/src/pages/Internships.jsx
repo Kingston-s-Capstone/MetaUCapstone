4705c67d32292eff5ff514f8698a25910367bb6f
Kingston Davies <kingstond22@meta.com>
2025-07-08 14:42:20 -0700

    logic for loading cards

--- frontend/src/pages/Internships.jsx
+++ frontend/src/pages/Internships.jsx
@@ -1,6 +1,44 @@
 import SearchForm from "../components/SearchForm"
+import { supabase } from "../SupaBaseClient"
+import { useState, useEffect } from "react"
 
 const Internships = () => {
+    const [internships, setInternships] = useState([])
+    const [page, setPage] = useState(1)
+    const [loading, setLoading] = useState(false)
+    const [sortBy, setSortBy] = useState("")
+
+    
+    const fetchInternships = async (reset = false) => {
+        setLoading(true);
+        const pageSize = 15;
+        const from = reset ? 0 : (page - 1) * pageSize;
+        const to = from + pageSize - 1;
+
+        let query = supabase
+            .from("internships")
+            .select("*")
+            .range(from, to);
+
+        if (sortBy === "newest") {
+            query = query.order("date_created", { ascending: false })
+        }
+
+        const { data, error } = await query;
+
+        if (error) {
+            console.error("Fetch error:", error)
+        } else {
+            setInternships(reset ? data : [...internships, ...data]);
+            if (!reset) setPage((prev) => prev + 1);
+        }
+
+        setLoading(false)
+    }
+    
+    useEffect(() => {
+        fetchInternships(true)
+    }, [sortBy])
     return (
         <div className="content">
             <header className="header">
@@ -9,7 +47,11 @@ const Internships = () => {
             <main className="main">
                 <div className="searchContainer">
                     <div className="filters">
-                        <select >
+                        <select onChange={(e) => {
+                            setSortBy(e.target.value);
+                            setPage(1);
+                            fetchInternships(true)
+                        }} >
                             <option value="">Recommended</option>
                             <option value="newest">Newly Added</option>
                             <option value="all">All Internships</option>
@@ -24,8 +66,17 @@ const Internships = () => {
                     </div>
                 </div>
                 <div className="internships">
-                    
+                    {internships.map((intern) => (
+                        <div key={intern.id} className="card">
+                            <a href={intern.url} target="_blank" rel="noreffer">{intern.title}</a>
+                            <span>{intern.organization}</span>
+                        </div>
+                    ))}
                 </div>
+
+                {!loading && (
+                    <button onClick={() => fetchInternships()} className="loadMore">Load More</button>
+                )}
             </main>
         </div>
     )
