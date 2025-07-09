ab5405777a67cd22367010559616b7550ca6accd
Kingston Davies <kingstond22@meta.com>
2025-07-09 14:51:24 -0700

    initial setup for recommendation tc

---
+++ python-service/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from recommend_engine import get_recommendations
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return jsonify({"message": "Flask is running"})
+
+@app.route('/recommendations/<user_id>')
+def recommendations(user_id):
+    results = get_recommendations(user_id)
+    return jsonify(results)
+
+if __name__ == '__main__':
+    app.run(port=5001, debug=True)
---
+++ python-service/recommend_engine.py
@@ -0,0 +1,5 @@
+def get_recommendations(user_id):
+    #placeholder logic
+    return [
+        {"type": "internship", "title": "Software Engineering intern", "score": 0.96}
+    ]
