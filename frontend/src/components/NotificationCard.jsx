2b064aa68e97ab456feaaa8929181f8075ec5067
Kingston Davies <kingstond22@meta.com>
2025-07-22 12:11:22 -0700

    create card component for notifications

---
+++ frontend/src/components/NotificationCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const NotificationCard = ({ notification, onClick }) => {
+    return (
+        <div
+            className={`notificationCard ${notification.status === 'unread' ? 'unread' : ''}`}
+            onClick={() => onClick(notification)}
+        >
+            <div className="notificationMessage">
+                {notification.message}
+            </div>
+            <div className="notificationTime">
+                {new Date(notification.created_at).toLocaleString()}
+            </div>
+        </div>
+    )
+}
+
+export default NotificationCard
