85a3cec7d03175054314e1f10dc9c2bb82526fe2
Kingston Davies <kingstond22@meta.com>
2025-07-23 15:43:18 -0700

    toast package files download

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from "./Router.jsx"
 import { AuthContextProvider } from './context/AuthContext.jsx'
+import { ToastContainer } from "react-toastify"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
