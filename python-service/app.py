d710f6ecd413927beeaa6da91b922bf33e2c27d9
Kingston Davies <kingstond22@meta.com>
2025-08-08 12:21:07 -0700

    changes to python service for deployment

--- python-service/app.py
+++ python-service/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from recommend_engine import get_recommendations
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,10 +10,15 @@ CORS(app)
 def home():
     return jsonify({"message": "Flask is running"})
 
+@app.get("/healthz")
+def healthz():
+    return jsonify(ok=True)
+
 @app.route('/recommendations/<user_id>', methods=['GET'])
 def recommendations(user_id):
     results = get_recommendations(user_id)
     return jsonify(results)
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host = "0.0.0.0", port=port, debug=True)
